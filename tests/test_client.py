6eb663d1d544be88a7f0e56d4cbc5210bb830414
Chris Fernando <chris.t.fernando@gmail.com>
2023-03-03 23:33:38 +1100

    Fixed tests and folder structure

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,22 +1,37 @@
 from bda_clock import client
 from datetime import datetime
-import json
+import unittest
 
-if client.has_clock("banana"):
-    print(f"Clock named banana exists")
-else:
-    print(f"Clock named banana does not exist - trying to make it...")
-    try:
-        new_clock = client.make_clock(
-            id="banana", start=datetime.now().astimezone(), interval_seconds=300
+
+class TestAll(unittest.TestCase):
+    def test_all(self):
+        fake_failed = client.has_clock("clock1-fake")
+        self.assertFalse(fake_failed)
+
+        client.make_clock(
+            "banana", start=datetime.now().astimezone(), interval_seconds=300
         )
-    except client.ClockCreateFailed as e:
-        print(f"Failed to create new clock. Error: {str(e)}")
-    else:
-        json_clock = json.loads(new_clock.text)
-        print(f"Created clock {json_clock['id']}")
+        real_success = client.has_clock("banana")
+        self.assertTrue(real_success)
 
-if client.has_clock("clock1"):
-    print(f"Clock named clock1 exists")
-else:
-    print(f"Clock named clock1 does not exist")
+
+"""
+        if client.has_clock("banana"):
+            print(f"Clock named banana exists")
+        else:
+            print(f"Clock named banana does not exist - trying to make it...")
+            try:
+                new_clock = client.make_clock(
+                    id="banana", start=datetime.now().astimezone(), interval_seconds=300
+                )
+            except client.ClockCreateFailed as e:
+                print(f"Failed to create new clock. Error: {str(e)}")
+            else:
+                json_clock = json.loads(new_clock.text)
+                print(f"Created clock {json_clock['id']}")
+
+        if client.has_clock("clock1"):
+            print(f"Clock named clock1 exists")
+        else:
+            print(f"Clock named clock1 does not exist")
+"""
