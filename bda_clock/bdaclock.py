c7d90feecf4f40e3a060141725f921516ded9de0
Chris Fernando <chris.t.fernando@gmail.com>
2023-03-04 12:51:40 +1100

    More imports

--- bda_clock/bdaclock.py
+++ bda_clock/bdaclock.py
@@ -2,7 +2,7 @@ import requests
 from datetime import datetime
 
 # from .config import urlbase
-from bda_clock import config
+from .config import *
 import json
 
 
@@ -14,21 +14,21 @@ class BdaClock:
 
     def __init__(self, id: str):
         self.id = id
-        response = requests.get(f"{config.urlbase}{id}")
+        response = requests.get(f"{urlbase}{id}")
         response_json = json.loads(response.text)
         self.now = response_json["now"]
         self.start = response_json["start"]
         self.interval = response_json["interval"]
 
     def tick(self):
-        url = f"{config.urlbase}{self.id}/tick"
+        url = f"{urlbase}{self.id}/tick"
         response = requests.put(url)
         response_json = json.loads(response.text)
         self.now = response_json["now"]
         return self.now
 
     def reset(self):
-        url = f"{config.urlbase}{self.id}/reset"
+        url = f"{urlbase}{self.id}/reset"
         response = requests.put(url)
         response_json = json.loads(response.text)
         self.now = response_json["now"]
