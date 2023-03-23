e0eadf6be6a05346bbea696c8d509544651ef900
Chris <57118805+chris-t-fernando@users.noreply.github.com>
2023-03-23 17:06:52 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="bda_clock",
-    version="1.13",
+    version="1.14",
     description="Client and Python object for interacting with BDA Clocks",
     author="Chris Fernando",
     author_email="chris.t.fernando@gmail.com",
